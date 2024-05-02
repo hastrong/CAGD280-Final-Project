dea764c648b0fdcaf7244b42fff44fc7c5f37161
hastrong <148916556+hastrong@users.noreply.github.com>
2024-05-02 11:20:51 -0700

    Enemy
    Created and started coding the enemy

---
+++ 280 Final Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Strong, Hannah]
+ * Date Last Modified: [05/02/2024]
+ * Codes for the enemy actions and interactions
+ */
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
